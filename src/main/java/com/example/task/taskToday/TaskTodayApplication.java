7cf89b16e22e351f2685e0dd390407ddeb1b6b07
PedroCardoso2 <www.pedrolucascardoso@gmail.com>
2024-03-14 18:24:56 -0300

    t-first

---
+++ src/main/java/com/example/task/taskToday/TaskTodayApplication.java
@@ -0,0 +1,13 @@
+package com.example.task.taskToday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTodayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTodayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/task/taskToday/TaskTodayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task.taskToday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTodayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
