22c6eb3b0c8cbeafb72aaf8f1a642abeaa4a24e9
PedroCardoso2 <www.pedrolucascardoso@gmail.com>
2024-03-16 15:12:05 -0300

    t-Usuario

--- src/main/java/com/example/task/taskToday/domain/entities/Tarefas.java
+++ src/main/java/com/example/task/taskToday/domain/entities/Tarefas.java
@@ -1,4 +1,24 @@
 package com.example.task.taskToday.domain.entities;
 
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "tarefas")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Tarefas {
+
+
+
 }
--- src/main/java/com/example/task/taskToday/domain/entities/Usuario.java
+++ src/main/java/com/example/task/taskToday/domain/entities/Usuario.java
@@ -1,10 +1,48 @@
 package com.example.task.taskToday.domain.entities;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.sql.Date;
+
 
 @Entity
-@Table
+@Table(name = "usuario")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Usuario {
+
+    @Column(name = "Id_Usu")
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(name = "nome_Usu")
+    @NotBlank
+    @Size(min = 2, max = 50)
+    private String nome;
+
+    @Column(name = "sobrenome_Usu")
+    @NotBlank
+    @Size(min = 2, max = 50)
+    private String sobreNome;
+
+    @NotBlank
+    private String senha;
+
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    @Column(name = "data_Nascimento")
+    @NotBlank
+    private Date dataNascimento;
+
 }
