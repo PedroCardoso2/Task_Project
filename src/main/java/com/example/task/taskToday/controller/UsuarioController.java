d9f36571d35463f8086231294c7664214f52d1d1
PedroCardoso2 <www.pedrolucascardoso@gmail.com>
2024-03-18 18:38:37 -0300

    t-Service and method

--- src/main/java/com/example/task/taskToday/controller/UsuarioController.java
+++ src/main/java/com/example/task/taskToday/controller/UsuarioController.java
@@ -7,6 +7,7 @@ import com.example.task.taskToday.domain.dtos.DadosAtualizacaoUsuario;
 import com.example.task.taskToday.domain.dtos.DadosUsuarioCadastrar;
 import com.example.task.taskToday.domain.dtos.DadosUsuarioSelecionado;
 import com.example.task.taskToday.domain.entities.Usuario;
+import com.example.task.taskToday.domain.service.UsuarioService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,29 +21,28 @@ public class UsuarioController {
     @Autowired
     private UsuarioRepository db;
 
+    @Autowired
+    private UsuarioService service;
+
 
     // Trazer Usuários (Código 200)
     @GetMapping("/{id}")
-    public ResponseEntity buscar(@PathVariable Long Id){
-        var usuario = db.getReferenceById(Id);
-
-        return ResponseEntity.ok(new DadosUsuarioSelecionado(usuario));
+    public ResponseEntity getUser(@PathVariable Long Id){
+        return service.buscar(Id);
     }
 
 
     // Cadastrar (Código 201)
     @PostMapping
-    public ResponseEntity inserirUsuario(@RequestBody @Valid DadosUsuarioCadastrar usu, UriComponentsBuilder uriComponentsBuilder){
-        var usuario = new Usuario(usu);
-        db.save(usuario);
-        var uri = uriComponentsBuilder.path("/cd-usu").buildAndExpand(usuario.getId()).toUri();
-
-        return ResponseEntity.created(uri).body(new DadosUsuarioSelecionado(usuario));
+    public ResponseEntity postUser(@RequestBody @Valid DadosUsuarioCadastrar usu, UriComponentsBuilder uriComponentsBuilder){
+        return service.inserirUsuario(usu, uriComponentsBuilder);
     }
+
+
 
     // Atualizar (Código 200)
     @PutMapping
-    public ResponseEntity atualizarDadosUsuario(@RequestBody @Valid DadosAtualizacaoUsuario usu){
+    public ResponseEntity putUser(@RequestBody @Valid DadosAtualizacaoUsuario usu){
         var usuario = db.getReferenceById(usu.id());
 
         usuario.atualizarDados(usu);
--- src/main/java/com/example/task/taskToday/domain/service/UsuarioService.java
+++ src/main/java/com/example/task/taskToday/domain/service/UsuarioService.java
@@ -1,8 +1,42 @@
 package com.example.task.taskToday.domain.service;
 
+import com.example.task.taskToday.domain.UsuarioRepository;
+import com.example.task.taskToday.domain.dtos.DadosUsuarioCadastrar;
+import com.example.task.taskToday.domain.dtos.DadosUsuarioSelecionado;
+import com.example.task.taskToday.domain.entities.Usuario;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
 public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository repository;
+
+
+    public ResponseEntity buscar(Long Id){
+        var usuario = repository.getReferenceById(Id);
+
+        return ResponseEntity.ok(new DadosUsuarioSelecionado(usuario));
+    }
+
+
+    public ResponseEntity inserirUsuario(DadosUsuarioCadastrar usu, UriComponentsBuilder uriComponentsBuilder){
+        var usuario = new Usuario(usu);
+        repository.save(usuario);
+
+        var uri = uriComponentsBuilder.path("/usu/table").buildAndExpand(usuario.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new DadosUsuarioSelecionado(usuario));
+    }
+
+
+
+
 
 }
