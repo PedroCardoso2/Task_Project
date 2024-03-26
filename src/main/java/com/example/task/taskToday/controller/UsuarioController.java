cd041461501197f40402f236e83007e4e950e6b5
PedroCardoso2 <www.pedrolucascardoso@gmail.com>
2024-03-26 18:35:26 -0300

    t-properties

--- src/main/java/com/example/task/taskToday/controller/UsuarioController.java
+++ src/main/java/com/example/task/taskToday/controller/UsuarioController.java
@@ -1,66 +1,66 @@
-package com.example.task.taskToday.controller;
+    package com.example.task.taskToday.controller;
 
 
-import com.example.task.taskToday.domain.UsuarioRepository;
+    import com.example.task.taskToday.domain.UsuarioRepository;
 
-import com.example.task.taskToday.domain.dtos.DadosAtualizacaoUsuario;
-import com.example.task.taskToday.domain.dtos.DadosUsuarioCadastrar;
-import com.example.task.taskToday.domain.dtos.DetalhamentoUsuarios;
-import com.example.task.taskToday.domain.service.UsuarioService;
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
+    import com.example.task.taskToday.domain.dtos.DadosAtualizacaoUsuario;
+    import com.example.task.taskToday.domain.dtos.DadosUsuarioCadastrar;
+    import com.example.task.taskToday.domain.dtos.DetalhamentoUsuarios;
+    import com.example.task.taskToday.domain.service.UsuarioService;
+    import jakarta.validation.Valid;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.data.domain.Page;
+    import org.springframework.data.domain.Pageable;
+    import org.springframework.data.web.PageableDefault;
+    import org.springframework.http.ResponseEntity;
+    import org.springframework.transaction.annotation.Transactional;
+    import org.springframework.web.bind.annotation.*;
+    import org.springframework.web.util.UriComponentsBuilder;
 
-@RestController
-@RequestMapping("/usuario")
-public class UsuarioController {
+    @RestController
+    @RequestMapping("/usuario")
+    public class UsuarioController {
 
-    @Autowired
-    private UsuarioRepository db;
+        @Autowired
+        private UsuarioRepository db;
 
-    @Autowired
-    private UsuarioService service;
+        @Autowired
+        private UsuarioService service;
 
 
-    // Trazer Usuários (Código 200)
-    @GetMapping
-    public ResponseEntity<Page<DetalhamentoUsuarios>> getUser(@PageableDefault Pageable pageable) {
-        return service.buscarTodos(pageable);
+        // Trazer Usuários (Código 200)
+        @GetMapping
+        public ResponseEntity<Page<DetalhamentoUsuarios>> getUser(@PageableDefault Pageable pageable) {
+            return service.buscarTodos(pageable);
+        }
+
+        @GetMapping("/{Id}")
+        public ResponseEntity getOnlyUser(@PathVariable Long Id){
+            return service.buscaUnica(Id);
+        }
+
+
+
+        // Cadastrar (Código 201)
+        @PostMapping
+        @Transactional
+        public ResponseEntity postUser(@RequestBody @Valid DadosUsuarioCadastrar usu, UriComponentsBuilder uriComponentsBuilder) {
+            return service.inserirUsuario(usu, uriComponentsBuilder);
+        }
+
+
+        // Atualizar (Código 200)
+        @PutMapping
+        @Transactional
+        public ResponseEntity putUser(@RequestBody @Valid DadosAtualizacaoUsuario usu) {
+            return service.atualizarUsuario(usu);
+        }
+
+        @DeleteMapping("/del/{Id}")
+        @Transactional
+        public ResponseEntity deleteUser(@PathVariable Long Id){
+            return service.deleteUser(Id);
+        }
+
+
     }
-
-    @GetMapping("/{Id}")
-    public ResponseEntity getOnlyUser(@PathVariable Long Id){
-        return service.buscaUnica(Id);
-    }
-
-
-
-    // Cadastrar (Código 201)
-    @PostMapping
-    @Transactional
-    public ResponseEntity postUser(@RequestBody @Valid DadosUsuarioCadastrar usu, UriComponentsBuilder uriComponentsBuilder) {
-        return service.inserirUsuario(usu, uriComponentsBuilder);
-    }
-
-
-    // Atualizar (Código 200)
-    @PutMapping
-    @Transactional
-    public ResponseEntity putUser(@RequestBody @Valid DadosAtualizacaoUsuario usu) {
-        return service.atualizarUsuario(usu);
-    }
-
-    @DeleteMapping("/del/{Id}")
-    @Transactional
-    public ResponseEntity deleteUser(@PathVariable Long Id){
-        return service.deleteUser(Id);
-    }
-
-
-}
