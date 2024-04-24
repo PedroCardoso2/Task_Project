e5ccebe8bd3557823eb7e46a1961a71e5ca83998
PedroCardoso2 <www.pedrolucascardoso@gmail.com>
2024-04-24 20:14:55 -0300

    Start Tarefas

---
+++ src/main/java/com/example/task/taskToday/domain/TarefaRepository.java
@@ -0,0 +1,4 @@
+package com.example.task.taskToday.domain;
+
+public interface TarefaRepository {
+}
--- src/main/java/com/example/task/taskToday/domain/entities/Tarefas.java
+++ src/main/java/com/example/task/taskToday/domain/entities/Tarefas.java
@@ -10,9 +10,33 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Date;
 
+@Entity
+@Table
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Tarefas {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id_Tarefas")
+    private Long idTar;
 
+    @Column(name = "nome_Taref")
+    @Size(max = 100)
+    private String descTar;
+
+    @Column(name = "feito")
+    private Enum statusTar;
+    @Column(name = "data_inicio_taref")
+    private Date inicTar;
+    @Column(name = "data_final_taref")
+    private Date FinTarf;
+
+    @Column(name = "")
+    private Usuario usuario;
 
 }
